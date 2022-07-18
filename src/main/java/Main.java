7aca3e60b7fe8cb6ab14874b6d798ed908eacde5
ChHong <pnci1029@gmail.com>
2022-07-18 18:59:03 +0900

    07.18

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        StringBuilder sb = new StringBuilder();
+        sb.append("하하호호\t");
+        sb.append("즐거운인생");
+        System.out.println(sb.toString());
+
     }
 }
