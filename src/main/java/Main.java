8ef657035a5c8cf56a628d1a001c28741a5fae21
ChHong <pnci1029@gmail.com>
2022-07-23 20:35:50 +0900

    07.21

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,19 @@
 // 달팽이 구하기
+class Solution {
+    public long solution(long n) {
+
+        System.out.println(n);
+
+        long answer = 0;
+        return answer;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("하하호호\t");
-        sb.append("즐거운인생");
-        System.out.println(sb.toString());
+        long n = 118372;
+        Solution sol = new Solution();
+        sol.solution(n);
 
     }
 }
