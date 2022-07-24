ca7e050f01c482ceda379c9733ba27d51cfd4ffd
ChHong <pnci1029@gmail.com>
2022-07-24 21:09:07 +0900

    07.24

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,36 @@
-// 달팽이 구하기
+import java.util.*;
 class Solution {
-    public long solution(long n) {
 
-        System.out.println(n);
+    public String solution(int[] numbers) {
+        String answer = "";
+        String result = "";
+        Arrays.sort(numbers);
+        String[] box = new String[10];
+//        System.out.println(box[9]);
 
-        long answer = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            result += String.valueOf(numbers[i]);
+        }
+
+        for (int i = 0; i < result.length(); i++) {
+            char c = result.charAt(i);
+            System.out.println(c);
+            if (c == '0') {
+                box[0] += c;
+            }
+        }
+        System.out.println(box[1]);
+
+
         return answer;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        long n = 118372;
+        int[] numbers = {6, 10, 2};
         Solution sol = new Solution();
-        sol.solution(n);
-
+        sol.solution(numbers);
+//            1 2 3 4 5 6  7
     }
 }
