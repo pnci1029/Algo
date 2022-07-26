77fc7eda659c848b50b6188eb74af5433fbff3bb
ChHong <pnci1029@gmail.com>
2022-07-26 19:21:51 +0900

    07.26

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,50 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        // 1. 중복 제거
+        HashSet<String> reportSet = new HashSet<String>();
+        for (String rep : report)
+            reportSet.add(rep);
 
-    public String solution(int[] numbers) {
-        String answer = "";
-        String result = "";
-        int a =0;
-        int bigOne = 0;
-        List<Integer> arr_01 = new ArrayList<>();
+        // 2. report에서 각 사람이 신고당한 횟수를 countHash으로 정의하기
+        HashMap<String, ArrayList<String>> notifyListHash = new HashMap<>();
+        for (String rep : reportSet) {
+            int blankIdx = rep.indexOf(" ");
+            String reporter = rep.substring(0, blankIdx);
+            String reportee = rep.substring(blankIdx + 1);
 
-        for (int n : numbers) {
-            if (n < 10) {
-                if (n > bigOne) {
-                    bigOne = n;
-                }
-            }
+            ArrayList<String> reporterList = notifyListHash.getOrDefault(reportee, null);
+            if (reporterList == null) reporterList = new ArrayList<>();
+
+            reporterList.add(reporter);
+            notifyListHash.put(reportee, reporterList);
         }
+
+        // 3. notifyListHash를 기반으로 reportHash만들기
+        HashMap<String, Integer> reporterHash = new HashMap<>();
+        for (ArrayList<String> notifyList : notifyListHash.values())
+            if (notifyList.size() >= k)
+                for (String reporter : notifyList)
+                    reporterHash.put(reporter, reporterHash.getOrDefault(reporter, 0) + 1);
+
+        // 4. reporterHash 정보를 answer에 옮겨담기
+        for (int i = 0; i < id_list.length; i++)
+            answer[i] = reporterHash.getOrDefault(id_list[i], 0);
+
         return answer;
     }
 }
 //65321110
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {6, 10, 2,13,51,97};
+        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        int k =2;
+
         Solution sol = new Solution();
-        sol.solution(numbers);
+        sol.solution(id_list, report, k);
 //            1 2 3 4 5 6  7
     }
 }
@@ -34,44 +53,3 @@ public class Main {
 
 
 
-
-//        for (int i = 0; i < numbers.length; i++) {
-//        result += numbers[i];
-//        }
-//
-//        String [] box = result.split("");
-//        String[] resultBox = {"", "", "", "", "", "", "", "", "", ""};
-//
-////        숫자 삽입
-//        for (int i = 0; i < box.length; i++) {
-//        int  boxInt = Integer.parseInt(box[i]);
-//        if (boxInt == 0) {
-//        resultBox[0]+=0;
-//        }else if(boxInt == 1) {
-//        resultBox[1] +=1;
-//        }else if(boxInt == 2) {
-//        resultBox[2] +=2;
-//        }else if(boxInt == 3) {
-//        resultBox[3]+=3;
-//        }else if(boxInt == 4) {
-//        resultBox[4]+=4;
-//        }else if(boxInt == 5) {
-//        resultBox[5]+=5;
-//        }else if(boxInt == 6) {
-//        resultBox[6]+=6;
-//        }else if(boxInt == 7) {
-//        resultBox[7]+=7;
-//        }else if(boxInt == 8) {
-//        resultBox[8]+=8;
-//        }else if(boxInt == 9) {
-//        resultBox[9]+=9;
-//        }
-//
-//        }
-//
-//        for (int i = resultBox.length - 1; i >= 0; i--) {
-//        if (resultBox[i] != null) {
-//        answer += resultBox[i];
-//        }
-//        }
-//        System.out.println(answer);
